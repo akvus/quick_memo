c10e13de21382094806658d2c47f8a63a91982f7
akvus <zawieja.mobile@gmail.com>
2025-02-02 10:48:09 +0100

    Improve layout

--- app/src/main/java/pl/akvus/quickmemo/screen/WordListScreen.kt
+++ app/src/main/java/pl/akvus/quickmemo/screen/WordListScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -26,7 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import pl.akvus.quickmemo.AddWordDialog
-import pl.akvus.quickmemo.ui.theme.QuickMemoTheme
 
 @Composable
 fun WordListScreen(
@@ -36,85 +36,97 @@ fun WordListScreen(
     val allWords by viewModel.allWords.observeAsState(initial = emptyList())
 
     var showAddDialog by remember { mutableStateOf(false) }
+    var searchQuery by remember { mutableStateOf("") }
 
-    QuickMemoTheme {
-        Column {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) {
-                items(allWords.size) { index ->
-                    val word = allWords[index]
-                    var showUpdateDialog by remember { mutableStateOf(false) }
+    val filteredWords = allWords.filter {
+        it.wordA.contains(searchQuery, ignoreCase = true) ||
+                it.wordB.contains(searchQuery, ignoreCase = true)
+    }
 
-                    Row(
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        verticalAlignment = Alignment.CenterVertically
+    Column {
+        TextField(
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+            modifier = Modifier
+                .fillMaxWidth(),
+            placeholder = { Text("Search...") }
+        )
 
-                    ) {
-                        Checkbox(
-                            checked = word.isLearned,
-                            onCheckedChange = { viewModel.updateWord(word.copy(isLearned = !word.isLearned)) }
-                        )
-                        Text(
-                            text = word.wordA + " - " + word.wordB,
-                            modifier = Modifier.weight(1f),
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        IconButton(onClick = {
-                            showUpdateDialog = true
-                        }) {
-                            Icon(Icons.Default.Edit, contentDescription = "Update")
-                        }
-                        IconButton(onClick = {
-                            viewModel.deleteWord(word)
-                        }) {
-                            Icon(Icons.Default.Delete, contentDescription = "Delete")
-                        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            items(filteredWords.size) { index ->
+                val word = filteredWords[index]
+                var showUpdateDialog by remember { mutableStateOf(false) }
 
-                        if (showUpdateDialog)
-                            AddWordDialog(
-                                word,
-                                onDismiss = { showUpdateDialog = false },
-                                onWordAdded = { wordA, wordB ->
-                                    viewModel.updateWord(word.copy(wordA = wordA, wordB = wordB))
-                                    showUpdateDialog = false
-                                }
-                            )
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Checkbox(
+                        checked = word.isLearned,
+                        onCheckedChange = { viewModel.updateWord(word.copy(isLearned = !word.isLearned)) }
+                    )
+                    Text(
+                        text = word.wordA + " - " + word.wordB,
+                        modifier = Modifier.weight(1f),
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    IconButton(onClick = {
+                        showUpdateDialog = true
+                    }) {
+                        Icon(Icons.Default.Edit, contentDescription = "Update")
                     }
-                }
-            }
-
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                TextButton(onClick = navigateToFlashcard) {
-                    Text("Start Learning")
-                }
-
-                TextButton(onClick = { showAddDialog = true }) {
-                    Text("Add Word")
-                }
-
-            }
-
-            if (showAddDialog) {
-                AddWordDialog(
-                    null,
-                    onDismiss = { showAddDialog = false },
-                    onWordAdded = { wordA, wordB ->
-                        viewModel.insertWord(wordA, wordB)
-                        showAddDialog = false
+                    IconButton(onClick = {
+                        viewModel.deleteWord(word)
+                    }) {
+                        Icon(Icons.Default.Delete, contentDescription = "Delete")
                     }
-                )
+
+                    if (showUpdateDialog)
+                        AddWordDialog(
+                            word,
+                            onDismiss = { showUpdateDialog = false },
+                            onWordAdded = { wordA, wordB ->
+                                viewModel.updateWord(word.copy(wordA = wordA, wordB = wordB))
+                                showUpdateDialog = false
+                            }
+                        )
+                }
             }
+        }
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            TextButton(onClick = navigateToFlashcard) {
+                Text("Start Learning")
+            }
+
+            TextButton(onClick = { showAddDialog = true }) {
+                Text("Add Word")
+            }
+
+        }
+
+        if (showAddDialog) {
+            AddWordDialog(
+                null,
+                onDismiss = { showAddDialog = false },
+                onWordAdded = { wordA, wordB ->
+                    viewModel.insertWord(wordA, wordB)
+                    showAddDialog = false
+                }
+            )
         }
     }
 }
