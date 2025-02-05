4b81b5c606e0064b8cf785becc3b03c956b2ab4f
akvus <zawieja.mobile@gmail.com>
2025-02-05 17:21:00 +0100

    Fix, when updating a word, set the current color as initial color

--- app/src/main/java/pl/akvus/quickmemo/screen/SaveWordDialog.kt
+++ app/src/main/java/pl/akvus/quickmemo/screen/SaveWordDialog.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import com.github.skydoves.colorpicker.compose.ColorEnvelope
@@ -48,6 +49,7 @@ fun SaveWordDialog(
                     label = { Text("Word B") }
                 )
                 HsvColorPicker(
+                    initialColor = word?.color?.let { Color(it) },
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(250.dp)
